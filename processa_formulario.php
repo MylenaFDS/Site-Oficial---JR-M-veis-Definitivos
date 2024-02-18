a77c9b72c1dd9477a9c6ae94a57313f0174ac5a5
Mylena Ferreira de Souza <mylenafds@gmail.com>
2024-02-17 21:42:25 -0300

    php

---
+++ processa_formulario.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Coletando os dados do formulário
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $mensagem = $_POST['mensagem'];
+
+    // Endereço de e-mail para onde a mensagem será enviada
+    $destinatario = "jrmoveisdefinitivos.com"; // Substitua pelo seu endereço de e-mail
+
+    // Construindo o corpo do e-mail
+    $assunto = "Nova mensagem de Fale Conosco";
+    $corpo_email = "Nome: $nome\n";
+    $corpo_email .= "Email: $email\n";
+    $corpo_email .= "Telefone: $telefone\n\n";
+    $corpo_email .= "Mensagem:\n$mensagem\n";
+
+    // Enviando o e-mail
+    mail($destinatario, $assunto, $corpo_email);
+
+    // Redirecionando de volta para a página do formulário
+    header("Location: index.html"); // Substitua "index.html" pelo nome do seu arquivo HTML de origem
+    exit();
+}
+?>
