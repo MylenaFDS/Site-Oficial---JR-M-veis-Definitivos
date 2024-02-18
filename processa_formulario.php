10f51d6d81dcb01aad24a820afda5e8de5961790
Mylena Ferreira de Souza <mylenafds@gmail.com>
2024-02-17 23:13:53 -0300

    PhPMailor

--- processa_formulario.php
+++ processa_formulario.php
@@ -1,4 +1,11 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer-PHPMailer-2128d99/src/Exception.php';
+require 'PHPMailer-PHPMailer-2128d99/src/PHPMailer.php';
+require 'PHPMailer-PHPMailer-2128d99/src/SMTP.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Coletando os dados do formulário
     $nome = $_POST['nome'];
@@ -6,20 +13,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $telefone = $_POST['telefone'];
     $mensagem = $_POST['mensagem'];
 
-    // Endereço de e-mail para onde a mensagem será enviada
-    $destinatario = "mylenafds@gmail.com"; // Substitua pelo seu endereço de e-mail
+    // Configuração do PHPMailer
+    $mail = new PHPMailer(true);
 
-    // Construindo o corpo do e-mail
-    $assunto = "Nova mensagem de Fale Conosco";
-    $corpo_email = "Nome: $nome\n";
-    $corpo_email .= "Email: $email\n";
-    $corpo_email .= "Telefone: $telefone\n\n";
-    $corpo_email .= "Mensagem:\n$mensagem\n";
+    // Configurações do servidor SMTP do Gmail
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'mylenafds@gmail.com'; // Substitua pelo seu endereço de e-mail
+    $mail->Password = 'FofuxaHta97'; // Substitua pela sua senha ou pela senha do aplicativo
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
 
-    // Enviando o e-mail
-    mail($destinatario, $assunto, $corpo_email);
+    // Remetente e destinatário
+    $mail->setFrom($email, $nome);
+    $mail->addAddress('mylenafds@gmail.com'); // Substitua pelo seu endereço de e-mail
 
-    // Redirecionando de volta para a página do formulário
+    // Conteúdo do e-mail
+    $mail->isHTML(true);
+    $mail->Subject = 'Nova mensagem de Fale Conosco';
+    $mail->Body = "Nome: $nome<br>Email: $email<br>Telefone: $telefone<br><br>Mensagem:<br>$mensagem";
+
+    // Tenta enviar o e-mail
+    try {
+        $mail->send();
+        echo 'E-mail enviado com sucesso';
+    } catch (Exception $e) {
+        echo 'Erro ao enviar o e-mail: ' . $mail->ErrorInfo;
+    }
+
+    // Redireciona de volta para a página do formulário
     header("Location: index.html"); // Substitua "index.html" pelo nome do seu arquivo HTML de origem
     exit();
 }
